585efe8a801b90c93c2157ab2a571030be338448
BraulioJose18 <40570187+BraulioJose18@users.noreply.github.com>
2022-10-14 01:54:16 -0500

    Structure of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ErrorPageComponent} from "./shared/error-page/error-page.component";
+import {SideMenuComponent} from "./shared/side-menu/side-menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full'},
+  {
+    path: 'auth',
+    loadChildren: ()=> import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'admin',
+    component: SideMenuComponent,
+    children: [
+      {
+        path: 'category',
+        loadChildren: ()=> import('./category/category.module').then(m => m.CategoryModule)
+      },
+      {
+        path: 'subcategory',
+        loadChildren: ()=> import('./subcategory/subcategory.module').then(m => m.SubcategoryModule)
+      },
+    ]
+  },
+  {
+    path: 'errorPage',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'errorPage',
+    pathMatch: 'full'
+  },
+
+  //{ path: '', redirectTo: 'login', pathMatch: 'full'},
+
+  //Rutas hijas
+ // { path: 'login', component: LoginComponent},
+  //{ path: 'navigation', loadChildren: ()=> import('./navigation/navigation.module').then(x => x.NavigationModule) },
+  //{ path: 'category', loadChildren: ()=> import('./pages/category/category.module').then(x => x.CategoryModule) },
+
+  //Redirecciona de ruta inexistente a ruta default
+ // { path: '**', redirectTo: 'login', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
